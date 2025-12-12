d48363282cc161a7e201f1a88919cda9a2afa9fd
JactusTheCactus <threateninglyamusing@gmail.com>
2025-12-11 18:27:25 -0800

    Update .sh

--- .sh
+++ .sh
@@ -8,5 +8,5 @@ flag() {
 export style="$(sass style.scss --style=compressed)"
 ./index.rb > ruby.html
 cat index.html | perl -pe '
-	s|<body>(.*?)</body>|$1|g;
+	s|^.*?<body>(.*?)</body>.*?$|$1|g;
 ' > README.md
