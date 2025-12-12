d19dd3da66af8fc358b6e68a365fc95c2b5dee8b
JactusTheCactus (PC) <threateninglyamusing@gmail.com>
2025-12-12 04:42:02 -0800

    -

--- .sh
+++ .sh
@@ -5,9 +5,24 @@ flag() {
 		do [[ -e ".flags/$f" ]] || return 1
 	done
 }
-export style="$(sass style.scss --style=compressed)"
-./index.rb > ruby.html
-cat index.html | perl -pe '
-	s|^[\S\s]*<body>||g;
-	s|</body>[\S\s]*$||g;
-' > README.md
+scss="$(mktemp)"
+trap "rm -f $scss" EXIT
+sass style.scss --style=compressed > "$scss" 2>& 1 || :
+export style="$(cat "$scss")"
+export break="{{SPLIT}}"
+ruby="$(mktemp)"
+trap "rm -f $ruby" EXIT
+./index.rb > $ruby
+re() {
+	a="[\S\s]*?"
+	s="$(echo "$break" | perl -pe 's|([{}])|\\$1|g')"
+	c="([\S\s]*?)"
+	w="\s*"
+	case $1 in
+		html)r="$c$w$s$a";;
+		md)r="$a$s$w$c";;
+	esac
+	perl -0777 -pe "s|^$r$|\$1|g"
+}
+cat $ruby | re html > index.html
+cat $ruby | re md > README.md
--- index.rb
+++ index.rb
@@ -11,10 +11,41 @@ E_ = "e%s" % [uni(0x0323)]
 MALE = uni 0x2642
 FEMALE = uni 0x2640
 NEUTER = uni 0x26A5
+def env(var)
+  ENV[var.to_s]
+end
+
 def filter(x)
   x
     .gsub("<", uni(0x3C))
     .gsub(">", uni(0x3E))
+end
+
+def getSex(sex = :neuter)
+  case sex
+  when :male then MALE
+  when :female then FEMALE
+  else NEUTER
+  end
+end
+
+def getInfo(form, label, list)
+  case form
+  when :html
+    [El.new(:dt, label),
+      El.new(:dd, El.new(:code,
+        if list.length > 1
+          El.new(:ol, *list.map { |s| El.new(:li, s.to_s) })
+        else
+          El.new(:span, list[0].to_s)
+        end))]
+  when :md
+    o = ["### #{label}"]
+    list.each { |i|
+      o.append "- #{i}"
+    }
+    o
+  end
 end
 
 class El
@@ -52,67 +83,55 @@ class Character
   def initialize(sex, name, pron, species, extra)
     @name = name
     @pron = pron
-    @sex = case sex
-    when :male
-      MALE
-    when :female
-      FEMALE
-    else
-      NEUTER
-    end
+    @sex = getSex(sex)
     @species = species
     @extra = extra
   end
 
   def to_el
-    El.new(:div,
-      El.new(:dl,
-        El.new(:dt,
-          El.new(:sup,
-            @sex),
-          @name.join(" ")),
-        El.new(:dd,
-          filter("<%s>" % [@pron.join("|")])),
-        El.new(:dt,
-          "Species"),
-        El.new(:dd,
-          El.new(:ol,
-            *@species.map { |s|
-              El.new(:li,
-                s.to_s)
-            })),
-        El.new(:dt,
-          "Extra"),
-        El.new(:dd,
-          El.new(:ul,
-            *@extra.map { |e|
-              El.new(:li,
-                e)
-            }))))
+    El.new(:dl,
+      El.new(:dt, El.new(:sup, @sex), @name.join(" ")),
+      El.new(:dd, filter("<%s>" % [@pron.join("|")])),
+      *getInfo(:html, :Species, @species),
+      *getInfo(:html, :Extra, @extra))
   end
 
   def to_s
     to_el.to_s
   end
 end
-puts El.new(:html,
-  El.new(:head,
-    El.new(:style,
-      ENV["style"])),
-  El.new(:body,
-    Character.new(:female,
-      ["Hound",
-        "NcNamara"],
-      ["haund",
-        "ny#{ACUTE}kn#{SCHWA}m#{E_}#{GRAVE}r#{SCHWA}"],
-      [:Reaper],
-      ["Shapeshifts Into a large, black wolf"]),
-    Character.new(:female,
-      ["Morrigan",
-        "Heffernan"],
-      ["m#{OMEGA}#{ACUTE}r#{SCHWA}gy#{GRAVE}n",
-        "he#{ACUTE}f#{SCHWA}rn#{ASH}#{GRAVE}n"],
-      [:Human,
-        :Changeling],
-      ["Killing Touch",
-        "wields a scythe"])))
+characters = [[:female,
+  ["Hound", "NcNamara"],
+  ["haund",
+    "ny%skn%sm%s%sr%s" % [ACUTE, SCHWA, E_, GRAVE, SCHWA]],
+  [:Human, :Changeling],
+  ["Shapeshifts into a large, black wolf"]],
+  [:female,
+    ["Morrigan", "Heffernan"],
+    ["m%s%sr%sgy%sn" % [OMEGA, ACUTE, SCHWA, GRAVE],
+      "he%sf%srn%s%sn" % [ACUTE, SCHWA, ASH, GRAVE]],
+    [:Reaper],
+    ["Killing touch", "Wields a scythe"]]]
+html = [El.new(:html,
+  El.new(:head, El.new(:style, env(:style))),
+  El.new(:body, *characters.map { |c| Character.new(*c) }))]
+characters.map! { |c|
+  {
+    sex: getSex(c[0]),
+    name: c[1].join(" "),
+    pron: filter("<%s>" % [c[2].join("|")]),
+    species: c[3].map { |s| s.to_s },
+    extra: c[4]
+  }
+}
+md = []
+characters.each { |c|
+  md.append "# #{c[:sex]}#{c[:name]}"
+  md.append "## #{c[:pron]}"
+  stats = [["Species", :species],
+    ["Extra", :extra]].map { |s|
+    getInfo(:md, s[0], c[s[1]])
+  }
+  md.append(*stats)
+}
+puts [*html, env(:break), *md]
